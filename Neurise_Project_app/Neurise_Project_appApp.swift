1a9c3c78c0f9d42dbb10a3ad3eab47fda2d17cd9
kiseong <ks.hwang.0313@gmail.com>
2026-05-14 20:49:13 +0900

    Initial Commit

---
+++ Neurise_Project_app/Neurise_Project_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neurise_Project_appApp.swift
+//  Neurise_Project_app
+//
+//  Created by 황기성 on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Neurise_Project_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
